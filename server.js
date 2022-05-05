85e37cd26a30fab46613ba269b6e4b1b2e328b2d
Yutthana00 <arf101293@gmail.com>
2022-05-06 00:36:33 +0100

    server layout setup with all required path

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = requires('./utils/helpers');
+
+// localHost port connection 3001
+const app = express(); 
+const PORT = process.env.PORT || 3001;
+
+// Handlebars custom helpers
+const hbs = exphbs.create({ helpers });
