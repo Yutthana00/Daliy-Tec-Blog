5eaed0ed8e05ee6ba01198d980713161427a8657
Yutthana00 <arf101293@gmail.com>
2022-05-06 00:38:16 +0100

    Express template engine select

--- server.js
+++ server.js
@@ -11,3 +11,17 @@ const PORT = process.env.PORT || 3001;
 
 // Handlebars custom helpers
 const hbs = exphbs.create({ helpers });
+
+// select which express.js template engine to use.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
