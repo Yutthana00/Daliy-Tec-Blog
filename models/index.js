3b18d68aed8e67edc1ad36c217491430003eeb2c
Yutthana00 <arf101293@gmail.com>
2022-05-08 00:16:45 +0100

    deleted extra ; and ,

--- models/index.js
+++ models/index.js
@@ -8,16 +8,17 @@ User.hasMany(Blog, {
 });
 
 Blog.belongsTo(User, {
-    foreignKey: 'user_id',
+    foreignKey: 'user_id'
 });
 
 Blog.hasMany(Comment, {
-    foreignKey: 'blog_id',
-});
+    foreignKey: 'blog_id'
+})
 
 Comment.belongsTo(User, {
     foreignKey: 'user_id',
-});
+})
 
 module.exports = { User, Blog, Comment };
+
 
