7d9f56269486a35fde206338fe1060f9926730b4
Yutthana00 <arf101293@gmail.com>
2022-05-08 04:50:16 +0100

    next added in cont

--- utils/auth.js
+++ utils/auth.js
@@ -1,10 +1,10 @@
-const withAuth = (req, res) => {
+const withAuth = (req, res, next) => {
 
     // Redirect to login page if user if is not logged in.
     if (!req.session.logged_in) {
         res.redirect('/login');
     } else {
-        next ();
+        next();
     }
 };
 
